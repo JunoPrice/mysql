26e521700eca98249944241cb54894d9de9407d8
JunoPrice <154485867+JunoPrice@users.noreply.github.com>
2025-01-08 01:49:12 -0300

    Update exercicio_02_ecomerse.sql

--- aula_01/exercicio_02_ecomerse.sql
+++ aula_01/exercicio_02_ecomerse.sql
@@ -26,39 +26,3 @@ SELECT * FROM tb_produtos;
 SELECT * FROM tb_produtos WHERE preco > 500;
 SELECT * FROM tb_produtos WHERE preco < 500;
 UPDATE tb_produtos SET preco = 1155 WHERE id = 1;
-
-
-
-
-
-
-
-
-
-CREATE TABLE tb_produtos(
-id bigint AUTO_INCREMENT,
-nome varchar(255) NOT NULL,
-quantidade int, 
-data_validade date,
-preco decimal NOT NULL, 
-PRIMARY KEY (id)
-);
-
--- Inserir dados na tabela
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("tomate",100, "2022-04-10", 8.00);
-
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("maçã",20, "2022-04-12", 5.00);
-
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("laranja",50, "2022-04-10", 10.00);
-
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("banana",200, "2022-04-13", 12.00);
-
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("uva",1200, "2022-04-14", 30.00);
-
-INSERT INTO tb_produtos(nome, quantidade, data_validade, preco)
-VALUES ("pêra",500, "2022-04-15", 2.90);
